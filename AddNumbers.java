c2881a4e33b667dcf4647a3f9afcae0a9c00f350
Priya <priya.chavan22@gmail.com>
2025-08-15 12:34:29 +0530

    some changes

--- AddNumbers.java
+++ AddNumbers.java
@@ -3,11 +3,12 @@ public class AddNumbers {
         // Declare and initialize two integer variables
         int number1 = 10;
         int number2 = 25;
+        int number3 = 30;
 
         // Add the two numbers
-        int sum = number1 + number2;
+        int sum = number1 + number2 + number3;
 
         // Print the result to the console
-        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
+        System.out.println("The sum of " + number1 + " and " + number2 + "and" + number3 + " is: " + sum);
     }
 }
